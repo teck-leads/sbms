ea3663c2ff659f221918205264f56caebddf99f5
TechLeads <anupoju.anand@gmail.com>
2020-07-16 08:29:35 +0530

    Configured Springboot Actuator Server UI
    1. Configured Springboot Actuator Server UI
    2. Configured Actuator client
    3. Get the Microservices actuators info in Springboot server UI

---
+++ spring-boot-admin-server/src/test/java/com/techleads/app/SpringBootAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techleads.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
