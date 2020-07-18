bc5cbef3da1eb9d735f4f3a053539e9559bb64e0
TechLeads <anupoju.anand@gmail.com>
2020-07-18 09:22:34 +0530

    Updated with Hystrix DashBoard
    @HystrixCommand(fallbackMethod = "paymentDetailsFallback",
    commandProperties = {
    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",
    value = "5"),
    @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds",
    value = "2000")})
    
    i) change to Hoxton.SR6 to Hoxton.SR3 or Hoxton.SR4 to make it work
    ii) more info: https://github.com/Netflix/Hystrix#readme

--- Payment-service-payment/src/main/java/com/techleads/app/PaymentServicePaymentApplication.java
+++ Payment-service-payment/src/main/java/com/techleads/app/PaymentServicePaymentApplication.java
@@ -5,14 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.HystrixCommands;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+import com.netflix.hystrix.HystrixCommand;
 @RefreshScope
 @EnableEurekaClient
 @SpringBootApplication
 @EnableHystrix
+@EnableHystrixDashboard
 public class PaymentServicePaymentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServicePaymentApplication.class, args);
+	
 	}
 
 }
--- Payment-service-payment/src/main/java/com/techleads/app/controller/PaymentController.java
+++ Payment-service-payment/src/main/java/com/techleads/app/controller/PaymentController.java
@@ -12,44 +12,51 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.techleads.app.model.PaymentProd;
 
 @RestController
 public class PaymentController {
 	@Value("${server.port}")
 	private int port;
-	
+	int p;
+	int f;
+
 	@GetMapping("/payment")
-	@HystrixCommand(fallbackMethod = "paymentDetailsFallback")
+	@HystrixCommand(fallbackMethod = "paymentDetailsFallback", commandProperties = {
+			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+			@HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds",value = "2000")})
 	public String paymentDetails() {
-		if(new Random().nextInt(10)<=10) {
+		System.out.println("paymentDetails() " + (++p));
+		if (new Random().nextInt(10) <= 10) {
 			throw new RuntimeException("paymentDetails is down");
 		}
-		return "From Payment-Service-Producer [Acutal response]: PORT-> "+port;
+		return "From Payment-Service-Producer [Acutal response]: PORT-> " + port;
 	}
-	
+
 	public String paymentDetailsFallback() {
-		return "From Payment-Service-Producer [@HystrixCommand(fallbackMethod = \"paymentDetailsFallback\")]: PORT-> "+port;
+		System.out.println("paymentDetailsFallback() " + (++f));
+		return "From Payment-Service-Producer [@HystrixCommand(fallbackMethod = \"paymentDetailsFallback\")]: PORT-> "
+				+ port;
 	}
-	
-	
+
 	@GetMapping("/payobj")
 	public PaymentProd paymentObj() {
-		return new PaymentProd(port,"p123",new Date());
+		return new PaymentProd(port, "p123", new Date());
 	}
-	
+
 	@GetMapping("/payobjs")
 	public List<PaymentProd> paymentObjs() {
-		return List.of(new PaymentProd(port,"p123",new Date()),new PaymentProd(port,"p1234",new Date()));
+		return List.of(new PaymentProd(port, "p123", new Date()), new PaymentProd(port, "p1234", new Date()));
 	}
 
 	@GetMapping("/payment/{id}")
-	public String getParam(@PathVariable("id")Integer id ) {
-		return "From Payment-Service-Producer path variable is "+id+" and: PORT-> "+port;
+	public String getParam(@PathVariable("id") Integer id) {
+		return "From Payment-Service-Producer path variable is " + id + " and: PORT-> " + port;
 	}
-	
+
 	@PostMapping("/payment")
 	public String savePayment(@RequestBody PaymentProd paymentProd) {
-		return "From Payment-Service-Producer path variable is "+paymentProd.toString()+" and: PORT-> "+port;
+		return "From Payment-Service-Producer path variable is " + paymentProd.toString() + " and: PORT-> " + port;
 	}
 }
